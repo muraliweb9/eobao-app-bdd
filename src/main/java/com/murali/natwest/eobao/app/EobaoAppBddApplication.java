04c1f9ef008677d4e76a7f4404dccc2245c2a8eb
muraliweb9 <muraliweb@hotmail.com>
2023-09-28 11:59:43 +0100

    Initial

---
+++ src/main/java/com/murali/natwest/eobao/app/EobaoAppBddApplication.java
@@ -0,0 +1,13 @@
+package com.murali.natwest.eobao.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EobaoAppBddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EobaoAppBddApplication.class, args);
+	}
+
+}
