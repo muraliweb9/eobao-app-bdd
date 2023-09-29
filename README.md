e8932efa20779b28e15ac9c06ba77a0b5d7eff6b
muraliweb9 <muraliweb@hotmail.com>
2023-09-29 12:41:21 +0100

    Initial

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # eobao-app-bdd
+
+### A simple REST based spring boot app
+Has 2 REST end points
+#### Get all the account types
+![doc_1](docs/doc_1.png)
+#### Get all the account types for a customer type
+![doc_2](docs/doc_2.png)
+![doc_3](docs/doc_3.png)
--- src/test/java/com/murali/natwest/eobao/EobaoAppTests.java
+++ src/test/java/com/murali/natwest/eobao/EobaoAppTests.java
@@ -1,10 +1,13 @@
 package com.murali.natwest.eobao;
 
-//@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 class EobaoAppTests {
 
-	//@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
